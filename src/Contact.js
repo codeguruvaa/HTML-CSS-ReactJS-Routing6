c4b0bcea57b84f3a54f2f9e65fbad81fc56bcda9
codeguruvaa <mohanvej@gmail.com>
2022-11-30 16:26:30 +0530

    init

---
+++ src/Contact.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
+
+function Contact() {
+    return (
+        <div>
+
+            <header>
+                <nav>
+                   <ul>
+                     <li><Link to="/" >Home</Link></li>
+                     <li><Link to="/about">About</Link></li>
+                     <li><Link to="/services">Services</Link></li>
+                     <li><Link to="/contact">Contact</Link></li>
+                   </ul>
+                </nav>
+            </header>
+
+
+            <section>
+                <h2>This is Contact Page</h2>
+                <p>
+                    the quick brown fox jumps voer the layz odgthe quick brown fox jumps voer the layz odg
+                    the quick brown fox jumps voer the layz odgthe quick brown fox jumps voer the layz odg
+                    the quick brown fox jumps voer the layz odgthe quick brown fox jumps voer the layz odg
+                    the quick brown fox jumps voer the layz odgthe quick brown fox jumps voer the layz odg
+                    the quick brown fox jumps voer the layz odgthe quick brown fox jumps voer the layz odg
+                    the quick brown fox jumps voer the layz odgthe quick brown fox jumps voer the layz odg
+                    the quick brown fox jumps voer the layz odgthe quick brown fox jumps voer the layz odg
+                </p>
+                <p>
+                    the quick brown fox jumps voer the layz odgthe quick brown fox jumps voer the layz odg
+                    the quick brown fox jumps voer the layz odgthe quick brown fox jumps voer the layz odg
+                    the quick brown fox jumps voer the layz odgthe quick brown fox jumps voer the layz odg
+                    the quick brown fox jumps voer the layz odgthe quick brown fox jumps voer the layz odg
+                    the quick brown fox jumps voer the layz odgthe quick brown fox jumps voer the layz odg
+                    the quick brown fox jumps voer the layz odgthe quick brown fox jumps voer the layz odg
+                    the quick brown fox jumps voer the layz odgthe quick brown fox jumps voer the layz odg
+                </p>
+            </section>
+
+        </div>
+    );
+}
+
+export default Contact;
